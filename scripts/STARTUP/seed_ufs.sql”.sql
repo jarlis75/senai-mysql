15bad4e5550a9dbabf47202a7277f662f9136643
jarlis <jharlis9@gmail.com>
2022-08-19 19:40:50 -0300

    adicionado arquivos de scripts starup

---
+++ scripts/STARTUP/seed_ufs.sql”.sql
@@ -0,0 +1,33 @@
+INSERT INTO uf (codigo_ibge, estado, ,sigla);
+ VALUES 
+(11, 'RONDÔNIA',' RO'),
+(12,' ACRE','AC'),
+(13,'AMAZONAS','AM'),
+(14,'RORAIMA','RR'),
+(15,'PARÁ','PA'),
+(16,'AMAPÁ','AP'),
+(17,'TOCANTINS','TO'),
+(21,'MARANHÃO','MA'),
+(22<'PIAUÍ','PI'),
+(23,'CEARÁ','CE'),
+(24,'RIO GRANDE DO NORTE','RN'),
+(25,'PARAÍBA','PB'),
+(26,'PERNAMBUCO','PE'),
+(27,'ALAGOAS','AL'),
+(28,'SERGIPE','SE'),
+(29,'BAHIA','BA'),
+(31,'MINAS GERAIS','MG'),
+(32,'ESPIRITO SANTO','ES'),
+(33,'RIO DE JANEIRO','RJ'),
+(35,'SÃO PAULO','SP'),
+(41,'PARANÁ','PR'),
+(42,'SANTA CATARINA','SC'),
+(43,'RIO GRANDE DO SUL','RS'),
+(50,'MATO GROSSO','MS'),
+(51,'MATO GROSSO','MT'),
+(52,'GOIÁS','GO'),
+(53,'DISTRITO','DF')
+
+
+
+
---
+++ scripts/seed_cidades.sq..sql
@@ -0,0 +1,12 @@
+INSERT INTO municipio ('municipio','uf')
+VALUES 
+('LINHARES','ES'),
+('COLATINA','ES'),
+('SÃO MATEUS','ES'),
+('JOÃO NEIVA','ES'),
+('PEDRO CANÁRIO','ES'),
+('RIO BANANAL','ES'),
+('FUNDÃO','ES'),
+('VILA VALÉRIO','ES'),
+('SERRA','ES'),
+('VITÓRIA','ES')
